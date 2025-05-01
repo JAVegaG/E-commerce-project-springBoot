2077d55da628c7b20a316fdd76fe55c85e94cf2c
Julian Vega <ja.vega.g10@gmail.com>
2025-05-01 08:52:07 -0500

    chore: fix warnings

--- JtProject/src/main/java/com/jtspringproject/JtSpringProject/dao/cartProductDao.java
+++ JtProject/src/main/java/com/jtspringproject/JtSpringProject/dao/cartProductDao.java
@@ -1,17 +1,18 @@
 package com.jtspringproject.JtSpringProject.dao;
 
-import com.jtspringproject.JtSpringProject.models.Cart;
-import com.jtspringproject.JtSpringProject.models.CartProduct;
-import com.jtspringproject.JtSpringProject.models.Product;
+import java.util.List;
+
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import com.jtspringproject.JtSpringProject.models.CartProduct;
+import com.jtspringproject.JtSpringProject.models.Product;
 
 @Repository
 public class cartProductDao {
+
     @Autowired
     private SessionFactory sessionFactory;
 
@@ -27,14 +28,14 @@ public class cartProductDao {
 
     @Transactional
     public List<CartProduct> getCartProducts() {
-        return this.sessionFactory.getCurrentSession().createQuery("from CART_PRODUCT ").list();
+        return this.sessionFactory.getCurrentSession().createQuery("from CART_PRODUCT ", CartProduct.class).list();
     }
 
     @Transactional
     public List<Product> getProductByCartID(Integer cart_id) {
         String sql = "SELECT product_id FROM cart_product WHERE cart_id = :cart_id";
         List<Integer> productIds = this.sessionFactory.getCurrentSession()
-                .createNativeQuery(sql)
+                .createNativeQuery(sql, Integer.class)
                 .setParameter("cart_id", cart_id)
                 .list();
 
--- JtProject/src/main/java/com/jtspringproject/JtSpringProject/models/User.java
+++ JtProject/src/main/java/com/jtspringproject/JtSpringProject/models/User.java
@@ -1,83 +1,77 @@
 package com.jtspringproject.JtSpringProject.models;
-import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-@Entity(name="CUSTOMER")
+@Entity(name = "CUSTOMER")
 @Table
 public class User {
 
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
-	@Column(unique = true)
-	private String username;
-	
-	private String email;
-	
-	private String password;
-	
-	private String role;
-	
-	private String address;
-	
-	
-	public int getId() {
-		return id;
-	}
+    @Column(unique = true)
+    private String username;
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    private String email;
 
-	public String getUsername() {
-		return username;
-	}
+    private String password;
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    private String role;
 
-	public String getEmail() {
-		return email;
-	}
+    private String address;
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public String getUsername() {
+        return username;
+    }
 
-	public String getRole() {
-		return role;
-	}
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-	public void setRole(String role) {
-		this.role = role;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public String getAddress() {
-		return address;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	
-	 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
 }
